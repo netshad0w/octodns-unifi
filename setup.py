87cf84866f101ddf280cec19d583d1ec9b529923
netshad0w <contact@nyxa.sh>
2026-04-05 03:41:00 +0200

    build: add requests dependency and update requirements

--- setup.py
+++ setup.py
@@ -20,12 +20,7 @@ def version():
 
 description, long_description = descriptions()
 
-tests_require = (
-    'pytest',
-    'pytest-cov',
-    'pytest-network',
-    # TODO: other test-time requirements
-)
+tests_require = ('pytest', 'pytest-cov', 'pytest-network')
 
 setup(
     author='netshad0w',
@@ -49,10 +44,7 @@ setup(
         ),
         'test': tests_require,
     },
-    install_requires=(
-        'octodns>=1.5.0',
-        # TODO: other requirements
-    ),
+    install_requires=('octodns>=1.5.0', 'requests>=2.27.0'),
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
