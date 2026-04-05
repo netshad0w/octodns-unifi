c8cdc083009c6d064f328d7bfee35a421159e23d
Mathieu <mathieu@nyxa.sh>
2026-04-05 03:34:45 +0200

    feat: import skeleton

---
+++ tests/test_provider_octodns_unifi.py
@@ -0,0 +1,14 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_unifi import UnifiProvider
+
+
+class TestUnifiProvider(TestCase):
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        UnifiProvider
